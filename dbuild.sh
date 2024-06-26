26015cc8e4d3a94d6922e65d31f1b06743a47109
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-06-26 21:11:37 +0200

    Release config

--- dbuild.sh
+++ dbuild.sh
@@ -7,8 +7,8 @@ DFLAGS+=" --linkonce-templates"
 DFLAGS+=" --preview=in"
 DFLAGS+=" --preview=bitfields"
 DFLAGS+=" --preview=fixImmutableConv"
-DFLAGS+=" --gc"
-# DFLAGS+=" --O3 --boundscheck=off"
+#DFLAGS+=" --gc"
+DFLAGS+=" --release --O3 --boundscheck=off"
 
 # Append default flags based on target
 DFLAGS+=" $(cd "$(dirname "${BASH_SOURCE[0]}")" && dflags.py)"
