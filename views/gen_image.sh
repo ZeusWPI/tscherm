4c94aabcbd2a8bbfa1072d5f142cef9d29018468
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-08-16 22:00:10 +0200

    Fix font generation after imagemagick update, drop font MSB (csync)

--- views/gen_image.sh
+++ views/gen_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Showing preview...
 magick $1 -resize '640x480!' png:- | feh -
-magick $1 -resize '640x480!' png:- | stream png:- -map r -storage-type char $2
+magick $1 -resize '640x480!' GRAY:$2
