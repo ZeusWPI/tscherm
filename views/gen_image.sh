7a0b569a89dcac3176abe7103476c48713a54750
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-08-16 20:42:12 +0200

    Magick cli changes, fix and regenerate images

--- views/gen_image.sh
+++ views/gen_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo Showing preview...
-convert -resize '640x480!' $1 png:- | feh -
-convert -resize '640x480!' $1 png:- | stream -map r -storage-type char png:- $2
+magick $1 -resize '640x480!' png:- | feh -
+magick $1 -resize '640x480!' png:- | stream png:- -map r -storage-type char $2
